0cfdfd281854ac51672ab059855330355d2fbc8f
Jonathan Perry <jonathanperry@modernpoacher.com>
2019-09-08 12:13:11 +0100

    `rimraf`

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
 //
-console.log(module.exports = require('./lib'))
-console.log(module.exports = require('./lib/redux'))
-console.log(module.exports = require('./lib/react-router'))
-console.log(module.exports = require('./lib/react-router/redux'))
-
 module.exports = require('./lib')
