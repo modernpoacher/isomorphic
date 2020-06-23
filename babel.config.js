79cd398af4662d5e78e2d0ae0f20805d6ffb4714
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-06-23 12:35:50 +0100

    `babel.config.js`

--- babel.config.js
+++ babel.config.js
@@ -1,28 +1,57 @@
-module.exports = {
-  compact: true,
-  comments: false,
-  presets: [
-    [
-      '@babel/env', {
-        targets: {
-          node: 'current'
-        },
-        useBuiltIns: 'usage',
-        corejs: 3
-      }
-    ],
-    '@babel/react'
+const debug = require('debug')
+
+const log = debug('@modernpoacher/isomorphic')
+
+const {
+  env: {
+    DEBUG = '@modernpoacher/isomorphic',
+    NODE_ENV = 'development'
+  }
+} = process
+
+debug.enable(DEBUG)
+
+log('`@modernpoacher/isomorphic` is awake')
+
+const presets = [
+  [
+    '@babel/env', {
+      targets: {
+        node: 'current'
+      },
+      useBuiltIns: 'usage',
+      corejs: 3
+    }
   ],
-  plugins: [
-    '@babel/proposal-export-default-from',
-    '@babel/proposal-export-namespace-from',
-    '@babel/proposal-class-properties',
-    [
-      'module-resolver', {
-        alias: {
-          '@modernpoacher/isomorphic': './src'
-        }
+  '@babel/react'
+]
+
+const plugins = [
+  '@babel/proposal-export-default-from',
+  '@babel/proposal-export-namespace-from',
+  '@babel/proposal-class-properties',
+  [
+    'module-resolver', {
+      alias: {
+        '@modernpoacher/isomorphic': './src'
       }
-    ]
+    }
   ]
+]
+
+function using () {
+  log({ NODE_ENV })
+
+  return NODE_ENV === 'production'
+}
+
+module.exports = (api) => {
+  if (api) api.cache.using(using)
+
+  return {
+    compact: true,
+    comments: false,
+    presets,
+    plugins
+  }
 }
