194fc7e2a5623cf50dad44017dc05e5b2499c124
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-06-24 17:26:26 +0100

    Babel

--- babel.config.js
+++ babel.config.js
@@ -4,12 +4,9 @@ const log = debug('@modernpoacher/isomorphic')
 
 const {
   env: {
-    DEBUG = '@modernpoacher/isomorphic',
     NODE_ENV = 'development'
   }
 } = process
-
-debug.enable(DEBUG)
 
 log('`@modernpoacher/isomorphic` is awake')
 
