50966c6cbf9e5d377a96dbfadc01224f767636a9
Jonathan Perry <jonathanperry@modernpoacher.com>
2024-11-29 13:58:14 +0000

    Mocha

--- test/react-router/index.spec.mjs
+++ test/react-router/index.spec.mjs
@@ -11,5 +11,5 @@ describe('`@modernpoacher/isomorphic/react-router`', () => {
 
   it('exports `renderToStaticMarkup`', () => expect(renderToStaticMarkup).to.be.a('function'))
 
-  xit('exports `render`', () => expect(render).to.be.a('function'))
+  it('exports `render`', () => expect(render).to.be.a('function'))
 })
--- test/redux/index.spec.mjs
+++ test/redux/index.spec.mjs
@@ -11,5 +11,5 @@ describe('`@modernpoacher/isomorphic/redux`', () => {
 
   it('exports `renderToStaticMarkup`', () => expect(renderToStaticMarkup).to.be.a('function'))
 
-  xit('exports `render`', () => expect(render).to.be.a('function'))
+  it('exports `render`', () => expect(render).to.be.a('function'))
 })
